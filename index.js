cbcf7e850ed2ade8720ef93e28119c4be550463e
Carlos Eduardo <65124135+KingTeox@users.noreply.github.com>
2022-08-26 14:47:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const Servidor = require("./server").HTTP;
+const http = require("http");
+const App = require("./server");
 const WS = require("./server-ws");
 const PORTA = process.env.PORT || 3000
 
-Servidor.listen({ port: PORTA });
+App.set("port", PORTA);
+
+let Servidor = http.createServer(App)
+
+Servidor.listen(PORTA);
+
+Servidor.on("listening", () => {
+    console.log(`[Teox] <Server> Online: ${PORTA}`); 
+    WS(Servidor);
+});
 
 Servidor.on("error", (error) => {
     console.log(`[Teox] <Server> Error: ${error}`);
 });
-
-WS(Servidor);
